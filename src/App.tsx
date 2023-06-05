6b447365787dac80e6110835d8129836d16e9cf6
DIFU-N <chukwudifunsofor@gmail.com>
2023-06-05 17:32:34 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+  // dont forget to run this one too.
+  // just restart the project
+  // npm install autoprefixer
+  //npm install --save-dev @types/react @types/react-dom
+  // npm install typescript @types/react @types/react-dom
+  
+  return (
+    <div>
+      <input type="search" id=""
+      className="h-9 border-black border-2 w-96"
+      />
+    </div>
+  )
+}
+
+export default App
